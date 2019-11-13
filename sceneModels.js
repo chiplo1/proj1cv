e66f6c5cde8281aaff2476b0d03a727b55346b35
chiplo1 <guilhermechiplo@hotmail.com>
2019-11-13 16:12:16 +0000

    start of project

---
+++ sceneModels.js
@@ -0,0 +1,275 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+//  For instantiating the scene models.
+//
+//  J. Madeira - November 2018
+//
+//////////////////////////////////////////////////////////////////////////////
+
+//----------------------------------------------------------------------------
+//
+//  Constructors
+//
+
+
+function emptyModelFeatures() {
+
+	// EMPTY MODEL
+
+	this.vertices = [];
+
+	this.normals = [];
+
+	// Transformation parameters
+
+	// Displacement vector
+	
+	this.tx = 0.0;
+	
+	this.ty = 0.0;
+	
+	this.tz = 0.0;	
+	
+	// Rotation angles	
+	
+	this.rotAngleXX = 0.0;
+	
+	this.rotAngleYY = 0.0;
+	
+	this.rotAngleZZ = 0.0;	
+
+	// Scaling factors
+	
+	this.sx = 1.0;
+	
+	this.sy = 1.0;
+	
+	this.sz = 1.0;		
+	
+	// Animation controls
+	
+	this.rotXXOn = true;
+	
+	this.rotYYOn = true;
+	
+	this.rotZZOn = true;
+	
+	this.rotXXSpeed = 1.0;
+	
+	this.rotYYSpeed = 1.0;
+	
+	this.rotZZSpeed = 1.0;
+	
+	this.rotXXDir = 1;
+	
+	this.rotYYDir = 1;
+	
+	this.rotZZDir = 1;
+	
+	// Material features
+	
+	this.kAmbi = [ 0.2, 0.2, 0.2 ];
+	
+	this.kDiff = [ 0.7, 0.7, 0.7 ];
+
+	this.kSpec = [ 0.7, 0.7, 0.7 ];
+
+	this.nPhong = 100;
+}
+
+function singleTriangleModel( ) {
+	
+	var triangle = new emptyModelFeatures();
+	
+	// Default model has just ONE TRIANGLE
+
+	triangle.vertices = [
+
+		// FRONTAL TRIANGLE
+		 
+		-0.5, -0.5,  0.5,
+		 
+		 0.5, -0.5,  0.5,
+		 
+		 0.5,  0.5,  0.5,
+	];
+
+	triangle.normals = [
+
+		// FRONTAL TRIANGLE
+		 
+		 0.0,  0.0,  1.0,
+		 
+		 0.0,  0.0,  1.0,
+		 
+		 0.0,  0.0,  1.0,
+	];
+
+	return triangle;
+}
+
+
+function simpleCubeModel( ) {
+	
+	var cube = new emptyModelFeatures();
+	
+	cube.vertices = [
+
+		-1.000000, -1.000000,  1.000000, 
+		 1.000000,  1.000000,  1.000000, 
+		-1.000000,  1.000000,  1.000000, 
+		-1.000000, -1.000000,  1.000000,
+		 1.000000, -1.000000,  1.000000, 
+		 1.000000,  1.000000,  1.000000, 
+         1.000000, -1.000000,  1.000000, 
+		 1.000000, -1.000000, -1.000000, 
+		 1.000000,  1.000000, -1.000000, 
+         1.000000, -1.000000,  1.000000, 
+         1.000000,  1.000000, -1.000000, 
+         1.000000,  1.000000,  1.000000, 
+        -1.000000, -1.000000, -1.000000, 
+        -1.000000,  1.000000, -1.000000,
+         1.000000,  1.000000, -1.000000, 
+        -1.000000, -1.000000, -1.000000, 
+         1.000000,  1.000000, -1.000000, 
+         1.000000, -1.000000, -1.000000, 
+        -1.000000, -1.000000, -1.000000, 
+		-1.000000, -1.000000,  1.000000, 
+		-1.000000,  1.000000, -1.000000, 
+		-1.000000, -1.000000,  1.000000, 
+		-1.000000,  1.000000,  1.000000, 
+		-1.000000,  1.000000, -1.000000, 
+		-1.000000,  1.000000, -1.000000, 
+		-1.000000,  1.000000,  1.000000, 
+		 1.000000,  1.000000, -1.000000, 
+		-1.000000,  1.000000,  1.000000, 
+		 1.000000,  1.000000,  1.000000, 
+		 1.000000,  1.000000, -1.000000, 
+		-1.000000, -1.000000,  1.000000, 
+		-1.000000, -1.000000, -1.000000,
+		 1.000000, -1.000000, -1.000000, 
+		-1.000000, -1.000000,  1.000000, 
+		 1.000000, -1.000000, -1.000000, 
+		 1.000000, -1.000000,  1.000000, 	 
+	];
+
+	computeVertexNormals( cube.vertices, cube.normals );
+
+	return cube;
+}
+
+
+function cubeModel( subdivisionDepth = 0 ) {
+	
+	var cube = new simpleCubeModel();
+	
+	midPointRefinement( cube.vertices, subdivisionDepth );
+	
+	computeVertexNormals( cube.vertices, cube.normals );
+	
+	return cube;
+}
+
+
+function simpleTetrahedronModel( ) {
+	
+	var tetra = new emptyModelFeatures();
+	
+	tetra.vertices = [
+
+		-1.000000,  0.000000, -0.707000, 
+         0.000000,  1.000000,  0.707000, 
+         1.000000,  0.000000, -0.707000, 
+         1.000000,  0.000000, -0.707000, 
+         0.000000,  1.000000,  0.707000, 
+         0.000000, -1.000000,  0.707000, 
+        -1.000000,  0.000000, -0.707000, 
+         0.000000, -1.000000,  0.707000, 
+         0.000000,  1.000000,  0.707000, 
+        -1.000000,  0.000000, -0.707000, 
+         1.000000,  0.000000, -0.707000, 
+         0.000000, -1.000000,  0.707000,
+	];
+
+	computeVertexNormals( tetra.vertices, tetra.normals );
+
+	return tetra;
+}
+
+
+function tetrahedronModel( subdivisionDepth = 0 ) {
+	
+	var tetra = new simpleTetrahedronModel();
+	
+	midPointRefinement( tetra.vertices, subdivisionDepth );
+	
+	computeVertexNormals( tetra.vertices, tetra.normals );
+	
+	return tetra;
+}
+
+
+function sphereModel( subdivisionDepth = 2 ) {
+	
+	var sphere = new simpleCubeModel();
+	
+	midPointRefinement( sphere.vertices, subdivisionDepth );
+	
+	moveToSphericalSurface( sphere.vertices )
+	
+	computeVertexNormals( sphere.vertices, sphere.normals );
+	
+	return sphere;
+}
+
+
+//----------------------------------------------------------------------------
+//
+//  Instantiating scene models
+//
+
+var sceneModels = [];
+
+// Model 0 --- Left
+
+sceneModels.push( new cubeModel( 1 ) );
+
+sceneModels[0].tx = -0.95; sceneModels[0].ty = 0; sceneModels[0].tz = 0;
+
+sceneModels[0].sx = 0.04; sceneModels[0].sy = 0.25 ; sceneModels[0].sz = 0.04;
+
+sceneModels[0].kDiff = [ 1.0, 0.0, 0.0 ];
+
+sceneModels[0].kSpec = [ 1.0, 0.0, 0.0 ];
+
+
+
+// Model 1 --- Right
+
+sceneModels.push( new cubeModel( 1 ) );
+
+sceneModels[1].tx = 0.95; sceneModels[1].ty = 0; sceneModels[1].tz = 0;
+
+sceneModels[1].sx = 0.04; sceneModels[1].sy = 0.25 ; sceneModels[1].sz = 0.04;
+
+sceneModels[1].kDiff = [ 0.0, 0.0, 1.0 ];
+
+sceneModels[1].kSpec = [ 0.0, 0.0, 1.0 ];
+
+
+
+// Model 2 --- Ball Middle
+
+sceneModels.push( new sphereModel( 3 ) );
+
+//sceneModels[2].tx = 0.5;
+
+sceneModels[2].sx = 0.05; sceneModels[2].sy = 0.1; sceneModels[2].sz = 0.05;
+
+sceneModels[2].kDiff = [ 0.0, 1.0, 0.0 ];
+
+sceneModels[2].kSpec = [ 0.0, 1.0, 0.0 ];
+
+
+
+
